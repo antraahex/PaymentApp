dbcf533ec4f43bd9505ed99a7cc8fbe4059f868a
antraahex <antra@ahex.co.in>
2021-02-10 22:29:02 +0530

    new changes

--- src/app/payment-confirmation/payment-confirmation.component.ts
+++ src/app/payment-confirmation/payment-confirmation.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-payment-confirmation',
@@ -8,21 +7,11 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class PaymentConfirmationComponent implements OnInit {
 
+  date: Date;
+  constructor() { }
 
-    isLinear = false;
-    firstFormGroup: FormGroup;
-    secondFormGroup: FormGroup;
-    date:Date;
-    constructor(private _formBuilder: FormBuilder) {}
-  
-    ngOnInit() {
-      this.firstFormGroup = this._formBuilder.group({
-        firstCtrl: ['', Validators.required]
-      });
-      this.secondFormGroup = this._formBuilder.group({
-        secondCtrl: ['', Validators.required]
-      });
-      this.date = new Date();
-    }
+  ngOnInit() {
+    this.date = new Date();
   }
+}
 
